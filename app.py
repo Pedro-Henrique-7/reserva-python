0472c54a4004e6babd6dd56064e1801a06027dd2
Pedro-Henrique-7 <phodasilva@gmail.com>
2025-05-29 16:05:28 -0300

    Sistema migrado para MySQL e recursos agora exibem status dinâmico conforme reservas existentes

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from database import listar_recursos, adicionar_recursos, reservar_recurso
-
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/') #cria rota inicial 
 def home():
-    recursos = listar_recursos()
-    return render_template('home.html', recursos=recursos)
+    data = request.args.get('data', date.today().isoformat())
+    recursos = listar_recursos(data)
+    return render_template('home.html', recursos=recursos, data=data )
 
 @app.route ('/adicionar-recurso', methods=['GET', 'POST']) #mostra formulario para adicionar um get e processa con post
 def adicionar_recurso_view():
@@ -20,13 +21,16 @@ def adicionar_recurso_view():
 
 @app.route ('/reservar', methods= ['GET', 'POST'])
 def reservar_view():
-    recursos = listar_recursos()
+    data = request.args.get('data', date.today().isoformat())
+    recursos = listar_recursos(data)
 
     if request.method == 'POST':
         recurso_id= request.form['recurso_id']
         usuario_nome = request.form['usuario_nome']
         data = request.form['data']
-        reservar_recurso(recurso_id, usuario_nome, data)
+        hora_inicio = request.form['hora_inicio']
+        hora_fim = request.form['hora_fim']
+        reservar_recurso(recurso_id, usuario_nome, data, hora_inicio, hora_fim)
         return redirect(url_for('home'))
     
     return render_template('reservar.html', recursos = recursos)
--- database.py
+++ database.py
@@ -1,70 +1,47 @@
-import sqlite3  # Importa o módulo para trabalhar com banco SQLite
+import mysql.connector
+from datetime import date
 
-def criar_banco():
-    # Abre conexão com o banco de dados (se o arquivo não existir, ele cria)
-    conn = sqlite3.connect('reserva_recursos.db')
-    cursor = conn.cursor()  # Cria um cursor para executar comandos SQL
+def conectar():
+    return mysql.connector.connect(
+        host="34.66.199.250", #banco de dados provisionado em nuvem googlcloud a fins de teste
+        user="pedro", 
+        password="123",
+        database="reserva_recursos"
+    )
 
-    # Cria a tabela 'recursos' caso não exista
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS recursos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,  # ID automático e único para cada recurso
-            nome TEXT NOT NULL,                    # Nome do recurso (ex: Sala 1)
-            descricao TEXT                        # Descrição opcional do recurso
-        )
-    ''')
+def listar_recursos(data):
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+    query = '''
+        SELECT r.id, r.nome, r.descricao,
+        CASE
+            WHEN EXISTS (
+                SELECT 1 FROM reservas res
+                WHERE res.recurso_id = r.id AND res.data = %s
+            ) THEN 'Reservado'
+            ELSE 'Disponível'
+        END AS status
+        FROM recursos r;            
 
-    # Cria a tabela 'reservas' caso não exista
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS reservas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,  # ID automático para cada reserva
-            recurso_id INTEGER NOT NULL,             # ID do recurso reservado (chave estrangeira)
-            usuario_nome TEXT NOT NULL,              # Nome do usuário que fez a reserva
-            data TEXT NOT NULL,                      # Data da reserva (formato string para simplicidade)
-            hora_inicio TEXT NOT NULL,               # Hora de início da reserva
-            hora_fim TEXT NOT NULL,                  # Hora de fim da reserva
-            status TEXT NOT NULL,                    # Status da reserva (ex: Confirmado)
-            FOREIGN KEY (recurso_id) REFERENCES recursos(id)  # Define relação com a tabela recursos
-        )
-    ''')
-
-    conn.commit()  # Salva as alterações feitas no banco
-    conn.close()   # Fecha a conexão com o banco
-
-
-def listar_recursos():
-    conn = sqlite3.connect('reserva_recursos.db')
-    cursor = conn.cursor()
-
-
-    cursor.execute('SELECT id, nome, descricao FROM recursos')
+    '''
+    cursor.execute(query,(data,))
     recursos = cursor.fetchall()
-
     conn.close()
     return recursos
 
-
 def adicionar_recursos(nome, descricao):
-    conn = sqlite3.connect('reserva_recursos.db')
-    cursor = conn.cursor()
-
-    cursor.execute('INSERT INTO recursos (nome, descricao) VALUES (?, ?)', (nome, descricao))
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("INSERT INTO recursos (nome, descricao) VALUES (%s, %s)", (nome, descricao))
     conn.commit()
     conn.close()
 
-def reservar_recurso(recurso_id, usuario_nome, data):
-    conn = sqlite3.connect('reserva_recursos.db')
+def reservar_recurso(recurso_id, usuario_nome, data, hora_inicio, hora_fim):
+    conn = conectar()
     cursor = conn.cursor()
-
-    cursor.execute('''
-        INSERT INTO reservas (recurso_id, usuario_nome, data)
-        VALUES (?, ?, ?)
-    ''', (recurso_id, usuario_nome, data))
-
+    cursor.execute("""
+        INSERT INTO reservas (recurso_id, usuario_nome, data, hora_inicio, hora_fim)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (recurso_id, usuario_nome, data, hora_inicio, hora_fim))
     conn.commit()
     conn.close()
-
-
-if __name__ == '__main__':
-    criar_banco()  # Executa a função que cria o banco e tabelas quando o script é rodado direto
-    print("Banco e tabelas criados com sucesso!")
